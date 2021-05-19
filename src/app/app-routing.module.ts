2df3f3119b467457496b593c7e8d19825a759d52
Eder Gonzalez <e113s10@gmail.com>
2021-05-19 13:55:23 -0500

    Page Not Found Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CountryFormComponent } from './pib/componentes/country-form/country-form.component';
 import { CountryComponent } from './pib/componentes/country/country.component';
 import { CountryDetailsComponent } from './pib/componentes/country-details/country-details.component';
+import { NotFoundComponent } from './pib/componentes/not-found/not-found.component';
 
 const routes: Routes = [
   {path: "", component: CountryComponent},
   {path: "edit/:code", component: CountryFormComponent},
-  {path: "countries/:code", component: CountryDetailsComponent}
+  {path: "countries/:code", component: CountryDetailsComponent},
+  {path: "**", component: NotFoundComponent}
 ];
 
 @NgModule({
