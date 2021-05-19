2ff7b23598c67ab655d9815ac566e3d934f6d676
Eder Gonzalez <e113s10@gmail.com>
2021-05-19 13:55:51 -0500

    Not Found Component Added

--- src/app/pib/pib.module.ts
+++ src/app/pib/pib.module.ts
@@ -6,13 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { CountryFormComponent } from './componentes/country-form/country-form.component';
 import { CountryDetailsComponent } from './componentes/country-details/country-details.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
 
 
 
 
 @NgModule({
-  declarations: [CountryComponent, CountryFormComponent, CountryDetailsComponent],
+  declarations: [CountryComponent, CountryFormComponent, CountryDetailsComponent, NotFoundComponent],
   imports: [CommonModule, NgxPaginationModule, FormsModule, AppRoutingModule],
-  exports: [CountryComponent, CountryFormComponent, CountryDetailsComponent]
+  exports: [CountryComponent, CountryFormComponent, CountryDetailsComponent, NotFoundComponent]
 })
 export class PibModule { }
